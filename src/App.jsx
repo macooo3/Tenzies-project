3e0ccc7c37420a71e08862c5a0251278f4bbf5df
macooo3 <marcoysu765@gmail.com>
2025-10-23 12:37:38 -0400

    comp for die

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,22 @@ import { useState } from "react";
 import Die from "./components/Die";
 
 function App() {
-  return <main>
-
-    <Die /> 
-
-  </main>;
-  
+  return (
+    <main>
+      <container className="die-container">
+        <Die value={3} />
+        <Die value={3} />
+        <Die value={3} />
+        <Die value={3} />
+        <Die value={3} />
+        <Die value={3} />
+        <Die value={3} />
+        <Die value={3} />
+        <Die value={3} />
+        <Die value={3} />
+      </container>
+    </main>
+  );
 }
 
 export default App;
