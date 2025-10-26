147d5d822a41ff5bcd8dac71edf60017c55ce524
macooo3 <marcoysu765@gmail.com>
2025-10-25 23:59:16 -0400

    created function to create 10 die

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,24 @@ import { useState } from "react";
 import Die from "./components/Die";
 
 function App() {
+  const generateAllNewDice = function () {
+    // const tenNumArr = [];
+    // for (let i = 0; i < 11; i++) {
+    //   const rand = Math.floor(Math.random() * 6 + 1);
+    //   tenNumArr.push(rand);
+    // }
+    // return tenNumArr;
+    return new Array(10).fill(0).map(() => Math.ceil(Math.random() * 6));
+  };
+
+  const [tenDie, setTenDie] = useState(generateAllNewDice())
+
+
   return (
     <main>
-      <container className="die-container">
-        <Die value={3} />
-        <Die value={3} />
-        <Die value={3} />
-        <Die value={3} />
-        <Die value={3} />
-        <Die value={3} />
-        <Die value={3} />
-        <Die value={3} />
-        <Die value={3} />
-        <Die value={3} />
-      </container>
+      <div className="die-container">
+     {tenDie.map((num, key) => <Die key={key}>{num}</Die>)}
+      </div>
     </main>
   );
 }
