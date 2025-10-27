5a97ef15fe34dd532d5c4368c912d14fa534af05
macooo3 <marcoysu765@gmail.com>
2025-10-27 02:34:27 -0400

    created obj for the dices and nned to fix conditon for dices

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Die from "./components/Die";
+import { nanoid } from "nanoid";
 
 function App() {
   const [dice, setDice] = useState(generateAllNewDice());
@@ -11,23 +12,34 @@ function App() {
     //   tenNumArr.push(rand);
     // }
     // return tenNumArr;
-    return new Array(10).fill(0).map(() => Math.ceil(Math.random() * 6));
+
+    return new Array(10)
+      .fill(0)
+      .map(() => ({
+        value: Math.ceil(Math.random() * 6),
+        isHeld: true,
+        id: nanoid(),
+      }));
   }
 
   function rollDices() {
     setDice(generateAllNewDice());
   }
 
-  const diceElements = dice.map((num, index) => (
-    <Die key={index} value={num} />
+  const diceElements = dice.map((dieObj) => (
+    <Die
+      style={{ backgroundColor: dieObj.isHeld && "#59E391" }}
+      key={dieObj.id}
+      value={dieObj.value}
+    />
   ));
 
   return (
     <main>
-      <div className="die-container">
-        {diceElements}
-      </div>
-        <button className="roll-button" onClick={rollDices}>Roll</button>
+      <div className="die-container">{diceElements}</div>
+      <button className="roll-button" onClick={rollDices}>
+        Roll
+      </button>
     </main>
   );
 }
